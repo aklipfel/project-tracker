8e87bab739662024abe4f18d3672901b2193bb32
aklipfel <contact@aklipfel.fr>
2020-10-17 14:20:56 +0200

    Remove boilerplate and import fonts

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
+@font-face {
+ font-family: "Rubik";
+ src: url("./assets/fonts/Rubik/Rubik-Regular.ttf") format("ttf");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-family:"Rubik";
   color: #2c3e50;
   margin-top: 60px;
 }
