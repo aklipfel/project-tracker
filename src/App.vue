d14cdd3628adfba882f113853a82f7ce9b94088d
aklipfel <contact@aklipfel.fr>
2020-10-17 15:08:36 +0200

    NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div id="red-bar"></div>
+    <Nav />
+    <router-view />
   </div>
 </template>
 
+
+<script>
+import Nav from "@/components/Nav";
+export default {
+  name: "App",
+  components: {
+    Nav,
+  },
+};
+</script>
+
+
 <style lang="scss">
+@import "@/assets/scss/_variables.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: inherit;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#red-bar {
+  height: 5vh;
+  width: 100%;
+  background-color: $darkred;
+}
+body {
+  margin: 0;
+  height: 95vh;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="nav">
+    <router-link to="/"><i class="fas fa-home fa-2x active"></i></router-link>
+    <router-link to="/about"><i class="fas fa-bell fa-2x"></i></router-link>
+    <router-link to="/about"
+      ><i class="fas fa-check-double fa-2x"></i
+    ></router-link>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/_variables.scss";
+#nav {
+  float: left;
+  display: flex;
+  flex-direction: column;
+  background-color: $darkgrey;
+  width: 7%;
+  height: 100%;
+
+  a {
+    color: $lightgrey;
+    text-decoration: none;
+    margin: 15px 0;
+  }
+
+  a:hover {
+    color: white;
+  }
+
+  .active {
+    background-color: $darkred;
+    border-radius: 5px;
+  }
+
+  i {
+    padding: 10px;
+  }
+}
+</style>
