85ec755770c0215f3751726b0ab348807a209e69
aklipfel <contact@aklipfel.fr>
2020-10-17 14:22:59 +0200

    Router & Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-  components: {
-  }
-}
-</script>
-
 <style lang="scss">
-@font-face {
- font-family: "Rubik";
- src: url("./assets/fonts/Rubik/Rubik-Regular.ttf") format("ttf");
-}
-
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  font-family:"Rubik";
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
